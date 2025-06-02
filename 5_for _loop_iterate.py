4f600c3920f65266cb6ca38896aaa7003e05ecc7
“Arpita <arpitakumari10653@gmail.com>
2025-06-02 11:53:53 +0530

    using iterate for loops

---
+++ 5_for _loop_iterate.py
@@ -0,0 +1,17 @@
+# (for loop ),with list
+l = [1, 23, 45, 76, 54, 456]
+
+for i in l:
+    print(i)
+
+#  (for loop ),with tuple
+
+t = (34, 89, 90, 78, 99)
+
+for i in t:
+    print(i)
+
+#  (for loop ),with sting    
+s = "Ram"
+for i in s:
+    print(i)
