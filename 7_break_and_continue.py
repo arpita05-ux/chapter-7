31c6e114fd9e71c8d1be16843cebcc8fa4a2f598
“Arpita <arpitakumari10653@gmail.com>
2025-06-02 17:56:05 +0530

    using break and continue statement

---
+++ 7_break_and_continue.py
@@ -0,0 +1,10 @@
+for i in range(100):
+   if(i == 33):
+    break # Exit the loop right now
+   print(i)
+
+for i in range(100):
+   if(i == 33):
+    continue # skip this iteration means skip 33 here
+   print(i)   
+ 
